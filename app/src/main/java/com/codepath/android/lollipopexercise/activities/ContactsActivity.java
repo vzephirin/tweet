99d840f092f50f6e3e27a6520870a80e43a995c3
grdollin <gregorydolin@yahoo.fr>
2018-02-25 21:34:21 -0500

    initialize

---
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/ContactsActivity.java
@@ -0,0 +1,86 @@
+package com.codepath.android.lollipopexercise.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.adapters.ContactsAdapter;
+import com.codepath.android.lollipopexercise.models.Contact;
+
+import java.util.List;
+
+
+public class ContactsActivity extends AppCompatActivity {
+    private RecyclerView rvContacts;
+    private ContactsAdapter mAdapter;
+    private List<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        // Find RecyclerView and bind to adapter
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // allows for optimizations
+        rvContacts.setHasFixedSize(true);
+
+        // Define 2 column grid layout
+        final GridLayoutManager layout = new GridLayoutManager(ContactsActivity.this, 2);
+
+        // Unlike ListView, you have to explicitly give a LayoutManager to the RecyclerView to position items on the screen.
+        // There are three LayoutManager provided at the moment: GridLayoutManager, StaggeredGridLayoutManager and LinearLayoutManager.
+        rvContacts.setLayoutManager(layout);
+
+        // get data
+        contacts = Contact.getContacts();
+
+        // Create an adapter
+        mAdapter = new ContactsAdapter(ContactsActivity.this, contacts);
+
+        // Bind adapter to list
+        rvContacts.setAdapter(mAdapter);
+       // Snackbar.make(getBaseContext(),);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addImage(MenuItem item) {
+
+       View.OnClickListener myOnClickListener =new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+           }
+
+       };
+
+        Snackbar.make(rvContacts, R.string.snackbar_text, Snackbar.LENGTH_LONG)
+                .setAction(R.string.snackbar_action, myOnClickListener)
+                .show();
+
+    }
+}
